a6790dafe16e4150f3f7c668aaddc20b58d0acb0
Ragnar20-03 <roshanpp20@gmail.com>
2023-07-09 08:58:27 +0530

    Write Sstem Call

--- FileSystem13.c
+++ FileSystem13.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<fcntl.h>
 #include<unistd.h>
 
 int main(int argc , char * argv[])
 {
     int fd =  0 ;
-    char Buffer [20] = {'\0'};
     int Ret =  0 ;
+    char * Buffer =  NULL;
 
-    if (argc != 2 )
+    if (argc != 3 )
     {
         printf("Insufficient Arguments \n");
         return -1;        
@@ -21,7 +22,10 @@ int main(int argc , char * argv[])
         return -1;
     }
 
-    Ret = read(fd , Buffer,10);
+    Buffer = (char * )malloc (sizeof (atoi(argv[2])));  // Dynamic Memory Allocation
+    
+
+    Ret = read(fd , Buffer, atoi( argv [2]));
     printf(" %d : ",Ret);
     if ( Ret == 0 )
     {
@@ -29,8 +33,11 @@ int main(int argc , char * argv[])
     }
     else
     {
-        printf("Data from file is : %s \n " , Buffer);
+        printf("Data from file is : \n  :%s \n " , Buffer);
     }
+
+    printf("Data From File is (Write): \n ");
+    write ( 1 , Buffer , Ret);
 
     return 0;
 }
