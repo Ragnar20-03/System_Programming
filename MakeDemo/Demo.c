462085b5a22e01aa76b4dc426e82b92b94974e6a
Ragnar20-03 <roshanpp20@gmail.com>
2023-09-10 15:21:20 +0530

    Make Files: Kernel Modukes

---
+++ MakeDemo/Demo.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(int argc  , char * argv[])
+{
+
+printf("Jay Ganesh : \n");
+printf("Jay Shree Mahakal : \n");
+printf("Jay Shree Ram : \n");
+
+if (argc == 2)
+{
+	printf("Welcome : %s \n",argv[1]);
+}
+
+
+return 0;
+}
